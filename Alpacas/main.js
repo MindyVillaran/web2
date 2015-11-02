958805a09754e665acc6977833f5bb29dfc6adbe
MindyVillaran <mindy.villaran@gmail.com>
2015-11-02 13:11:52 -0500

    added scrolling effects

--- Alpacas/main.js
+++ Alpacas/main.js
@@ -1,5 +1,16 @@
+$(".scroller").scrollFlight();
 
+$(".title").on("arrived",function() {
+  $(".title").addClass("title-arrived");
+});
+
+$(".text-block").on("arrived",function() {
+  $(this).addClass("text-block-arrived");
+}); 
   
-  
+$(".text-block2").on("arrived",function() {
+  $(this).addClass("quote-arrived");
+}); 
+
 
 
