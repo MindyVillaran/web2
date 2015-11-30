9b6050357c94085dbb8f9ea437266eb383fffc83
MindyVillaran <mindy.villaran@gmail.com>
2015-11-30 13:41:08 -0500

    began creating dynamic content

--- Jewelry-Collection/main.js
+++ Jewelry-Collection/main.js
@@ -1,29 +1,31 @@
-var $container = $("#grid-container");  
+function addItem(item){    //need to create a function to access the data. "item" accesses the data sheet
 
+var title = item.Name;  //creating variables with "item.Name", etc. 
+var	image = item.Image
+
+var $container = $("#grid-container");  // create new variables to include and later append to the box
 var $box = $("<div class='box'>"); 
-var $img = $("<img class='image'>").attr("src","images/img15.JPG");  //and add in static values
+var $img = $("<img class='image'>").attr("src",image); //pass in the variables for "item.Name"   
 var $overlay = $("<div class='imgOvelay'>")
-var $h2 = $("<h2>");  //create new variables 
-var $title = $("<span class='title'>").text("Fancy Title");  //create new variables 
-
+var $h2 = $("<h2>");  
+var $title = $("<span class='title'>").text(title);  //pass in the variables for "item.Title"  
 
 $h2.append($title)
 $overlay.append($h2)
 $box.append($img, $overlay)
 $container.append($box)
+}
 
 
 
-var $container = $("#grid-container");  
+function callback(data) { 
+	console.log(data);	
+	addItem(data[0]);
+	addItem(data[1]);	
+	addItem(data[3]);
+	addItem(data[4]);
+		addItem(data[8]);
+}
 
-var $box = $("<div class='box'>"); 
-var $img = $("<img class='image'>").attr("src","images/img16.JPG");  //and add in static values
-var $overlay = $("<div class='imgOvelay'>")
-var $h2 = $("<h2>");  //create new variables 
-var $title = $("<span class='title'>").text("Fancy Title");  //create new variables 
 
 
-$h2.append($title)
-$overlay.append($h2)
-$box.append($img, $overlay)
-$container.append($box)
